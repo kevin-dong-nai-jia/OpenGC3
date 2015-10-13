b8519b1753396050ae6adf8d240e97c208d90231
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2015-10-14 02:32:41 +0800

    initial commit

---
+++ tests/deque-test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../ccc/deque.h"
+
+
+int main(void)
+{
+    /* (b)oundary-test: cc_deque_push_back */
+    {
+        // TODO
+    }
+
+    /* (o)rdinary-test: cc_deque_push_back */
+    {
+        // TODO
+    }
+
+    return 0;
+}
