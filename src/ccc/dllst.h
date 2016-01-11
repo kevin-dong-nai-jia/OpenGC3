4ea4ee8f811e33b3fd4e9b99fba611ad3e0ce5d7
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-01-12 04:34:02 +0800

    update dllst.h and dllst-test.h

---
+++ src/ccc/dllst.h
@@ -0,0 +1,216 @@
+#ifndef _CCC_DLLST_H_
+#define _CCC_DLLST_H_
+
+
+
+/* dllst container struct */
+
+
+#define _cc_dllst_init {0, NULL, NULL, NULL}
+
+#define cc_dllst(_cc_dllst_object,                                         \
+                 _cc_dllst_element_type)                                   \
+                                                                           \
+typedef _cc_dllst_element_type _cc_dllst_object##_element_type;            \
+                                                                           \
+struct                                                                     \
+{                                                                          \
+    int size;                                                              \
+    void *head, *tail, *avsp;                                              \
+                                                                           \
+}   _cc_dllst_object = _cc_dllst_init
+
+
+#define _cc_dllst_node(_cc_dllst_object)                                   \
+                                                                           \
+struct                                                                     \
+{                                                                          \
+    _cc_dllst_object##_element_type val;                                   \
+                                                                           \
+    void *pitn[4];                                                         \
+}
+
+
+
+/* dllst container iterators */
+
+
+#define cc_dllst_iter(_cc_dllst_iter,                                      \
+                      _cc_dllst_object)                                    \
+                                                                           \
+_cc_dllst_object##_element_type** _cc_dllst_iter = NULL
+
+
+#define cc_dllst_iter_begin(_cc_dllst_object)                              \
+(                                                                          \
+    (_cc_dllst_object.head == NULL) ?                                      \
+    (NULL) :                                                               \
+    ((_cc_dllst_object##_element_type**)_cc_dllst_object.head + 2)         \
+)
+
+
+#define cc_dllst_iter_end(_cc_dllst_object)                                \
+(                                                                          \
+    (_cc_dllst_object.tail == NULL) ?                                      \
+    (NULL) :                                                               \
+    ((_cc_dllst_object##_element_type**)_cc_dllst_object.tail - 1)         \
+)
+
+
+#define cc_dllst_iter_is_valid(_cc_dllst_iter)                             \
+(                                                                          \
+    _cc_dllst_iter != NULL && *_cc_dllst_iter != NULL                      \
+)
+
+
+#define cc_dllst_iter_incr(_cc_dllst_iter)                                 \
+(                                                                          \
+    (*(_cc_dllst_iter + 1) == NULL) ?                                      \
+    (_cc_dllst_iter = NULL) :                                              \
+    (_cc_dllst_iter = (void*)(*((void***)_cc_dllst_iter + 1) + 2))         \
+)
+
+
+#define cc_dllst_iter_decr(_cc_dllst_iter)                                 \
+(                                                                          \
+    (*(_cc_dllst_iter - 2) == NULL) ?                                      \
+    (_cc_dllst_iter = NULL) :                                              \
+    (_cc_dllst_iter = (void*)(*((void***)_cc_dllst_iter - 2) - 1))         \
+)
+
+
+
+/* dllst container traversal */
+
+
+#define cc_dllst_trav(_cc_dllst_iter,                                      \
+                      _cc_dllst_object)                                    \
+                                                                           \
+for                                                                        \
+(                                                                          \
+    _cc_dllst_iter = cc_dllst_iter_begin(_cc_dllst_object);                \
+    cc_dllst_iter_is_valid(_cc_dllst_iter);                                \
+    cc_dllst_iter_incr(_cc_dllst_iter)                                     \
+)
+
+
+#define cc_dllst_rtrav(_cc_dllst_iter,                                     \
+                       _cc_dllst_object)                                   \
+                                                                           \
+for                                                                        \
+(                                                                          \
+    _cc_dllst_iter = cc_dllst_iter_end(_cc_dllst_object);                  \
+    cc_dllst_iter_is_valid(_cc_dllst_iter);                                \
+    cc_dllst_iter_decr(_cc_dllst_iter)                                     \
+)
+
+
+
+/* dllst container capacity */
+
+
+#define cc_dllst_size(_cc_dllst_object)                                    \
+(                                                                          \
+    _cc_dllst_object.size                                                  \
+)
+
+
+#define cc_dllst_empty(_cc_dllst_object)                                   \
+(                                                                          \
+    cc_dllst_size(_cc_dllst_object) == 0                                   \
+)
+
+
+#define cc_dllst_resize(_cc_dllst_object,                                  \
+                        _cc_dllst_padding_value)                           \
+    /* TODO */
+
+
+
+/* dllst container modifiers */
+
+
+#define cc_dllst_push_back(_cc_dllst_object,                               \
+                           _cc_dllst_push_back_value)                      \
+{                                                                          \
+    _cc_dllst_node(_cc_dllst_object) *new_node =                           \
+        malloc(sizeof(_cc_dllst_node(_cc_dllst_object)));                  \
+                                                                           \
+    new_node->val     = _cc_dllst_push_back_value;                         \
+    new_node->pitn[0] = NULL;                                              \
+    new_node->pitn[1] = new_node;                                          \
+    new_node->pitn[2] = &(new_node->val);                                  \
+    new_node->pitn[3] = NULL;                                              \
+                                                                           \
+    if (cc_dllst_empty(_cc_dllst_object))                                  \
+        _cc_dllst_object.head = &(new_node->pitn[0]);                      \
+    else                                                                   \
+        new_node->pitn[0] = _cc_dllst_object.tail,                         \
+        *(void**)(_cc_dllst_object.tail) = &(new_node->pitn[0]);           \
+                                                                           \
+    _cc_dllst_object.tail = &(new_node->pitn[3]);                          \
+    _cc_dllst_object.size++;                                               \
+}
+
+
+#define cc_dllst_push_front(_cc_dllst_object,                              \
+                            _cc_dllst_push_front_value)                    \
+{                                                                          \
+    _cc_dllst_node(_cc_dllst_object) *new_node =                           \
+        malloc(sizeof(_cc_dllst_node(_cc_dllst_object)));                  \
+                                                                           \
+    new_node->val     = _cc_dllst_push_front_value;                        \
+    new_node->pitn[0] = NULL;                                              \
+    new_node->pitn[1] = new_node;                                          \
+    new_node->pitn[2] = &(new_node->val);                                  \
+    new_node->pitn[3] = NULL;                                              \
+                                                                           \
+    if (cc_dllst_empty(_cc_dllst_object))                                  \
+        _cc_dllst_object.tail = &(new_node->pitn[3]);                      \
+    else                                                                   \
+        new_node->pitn[3] = _cc_dllst_object.head,                         \
+        *(void**)(_cc_dllst_object.head) = &(new_node->pitn[3]);           \
+                                                                           \
+    _cc_dllst_object.head = &(new_node->pitn[0]);                          \
+    _cc_dllst_object.size++;                                               \
+}
+
+
+#define cc_dllst_pop_back(_cc_dllst_object)                                \
+    /* TODO */
+
+
+#define cc_dllst_pop_front(_cc_dllst_object)                               \
+    /* TODO */
+
+
+#define cc_dllst_clear(_cc_dllst_object)                                   \
+    /* TODO */
+
+
+
+/* dllst container deallocation */
+
+
+#define cc_dllst_dealloc(_cc_dllst_object)                                 \
+{                                                                          \
+    cc_dllst_iter(iter, _cc_dllst_object);                                 \
+    cc_dllst_iter(iter_dup, _cc_dllst_object);                             \
+    iter = cc_dllst_iter_begin(_cc_dllst_object);                          \
+                                                                           \
+    while (cc_dllst_iter_is_valid(iter))                                   \
+    {                                                                      \
+        iter_dup = iter;                                                   \
+        cc_dllst_iter_incr(iter);                                          \
+        free(*(iter_dup - 1));                                             \
+    }                                                                      \
+                                                                           \
+    _cc_dllst_object.tail = NULL;                                          \
+    _cc_dllst_object.head = NULL;                                          \
+    _cc_dllst_object.avsp = NULL;                                          \
+    _cc_dllst_object.size = 0;                                             \
+}
+
+
+
+#endif
--- tests/dllst-test.c
+++ tests/dllst-test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../src/ccc/deque.h"
+#include "../src/ccc/dllst.h"
 
 
 int main(void)
@@ -12,17 +12,17 @@ int main(void)
     printf("Test 0:\n\n");
 
     {
-        cc_deque(msg, char*);
+        cc_dllst(msg, char*);
 
-        cc_deque_push_back(msg, "Hello everyone:\n");
-        cc_deque_push_back(msg, "Welcome to C Container Collection Project!\n");
+        cc_dllst_push_back(msg, "Hello everyone:\n");
+        cc_dllst_push_back(msg, "Welcome to C Container Collection Project!\n");
 
         {
-            cc_deque_iter(msg_iter, msg);
-            cc_deque_trav(msg_iter, msg)  printf("%s", **msg_iter);
+            cc_dllst_iter(msg_iter, msg);
+            cc_dllst_trav(msg_iter, msg)  printf("%s", **msg_iter);
         }
 
-        cc_deque_dealloc(msg);
+        cc_dllst_dealloc(msg);
     }
 
 
@@ -32,7 +32,7 @@ int main(void)
     printf("\n\nTest 1:");
 
     {
-        cc_deque(test1, char*);
+        cc_dllst(test1, char*);
 
         char *num_str[10] = { "five" , "six", "four", "seven", "three",
                               "eight", "two", "nine", "one"  , "ten"    };
@@ -41,17 +41,17 @@ int main(void)
 
         while (puts("") && cnt <= 10)
         {
-            cc_deque_iter(test1_iter, test1);
-            test1_iter = cc_deque_iter_begin(test1);
+            cc_dllst_iter(test1_iter, test1);
+            test1_iter = cc_dllst_iter_begin(test1);
 
-            cc_deque_trav(test1_iter, test1)
+            cc_dllst_trav(test1_iter, test1)
                 printf("%s ", **test1_iter);
 
-            cc_deque_push_front(test1, num_str[cnt++]);
-            cc_deque_push_back (test1, num_str[cnt++]);
+            cc_dllst_push_front(test1, num_str[cnt++]);
+            cc_dllst_push_back (test1, num_str[cnt++]);
         }
 
-        cc_deque_dealloc(test1);
+        cc_dllst_dealloc(test1);
     }
 
 
@@ -61,14 +61,14 @@ int main(void)
     printf("\n\nTest 2: (no output)\n");
 
     {
-        cc_deque(test2, int);
+        cc_dllst(test2, int);
 
         int cnt = 0;
 
         while (cnt <= 10000000)
-            cc_deque_push_back(test2, cnt++);
+            cc_dllst_push_back(test2, cnt++);
 
-        cc_deque_dealloc(test2);
+        cc_dllst_dealloc(test2);
     }
 
 
@@ -83,8 +83,8 @@ int main(void)
             char *msg[2];
         } test3_struct;
 
-        cc_deque(test3, test3_struct);
-        cc_deque_iter(test3_iter, test3);
+        cc_dllst(test3, test3_struct);
+        cc_dllst_iter(test3_iter, test3);
 
         test3_struct test3_1, test3_2;
 
@@ -93,14 +93,14 @@ int main(void)
         test3_2.msg[0] = "two";
         test3_2.msg[1] = "structs.\n";
 
-        cc_deque_push_back(test3, test3_1);
-        cc_deque_push_back(test3, test3_2);
+        cc_dllst_push_back(test3, test3_1);
+        cc_dllst_push_back(test3, test3_2);
 
-        cc_deque_trav(test3_iter, test3)
+        cc_dllst_trav(test3_iter, test3)
             printf("%s ", (**test3_iter).msg[0]),
             printf("%s ", (**test3_iter).msg[1]);
 
-        cc_deque_dealloc(test3);
+        cc_dllst_dealloc(test3);
     }
 
 
