14990f8c3c9c04158799ce0b1d25124aab089dde
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-01-13 00:39:55 +0800

    add cc_sllst and _cc_xllst_node

--- src/ccc/sllst.h
+++ src/ccc/sllst.h
@@ -3,8 +3,40 @@
 
 
 
+/* xllst container struct */
+/* xllst = sllst or dllst */
+
+
+#define _cc_xllst_node(_cc_xllst_object)                                   \
+                                                                           \
+struct                                                                     \
+{                                                                          \
+    void *ptn[3];                                                          \
+    _cc_xllst_object##_element_type val;                                   \
+}
+
+
+
 /* sllst container struct */
 
 
+#define _cc_sllst_init {0, NULL}
 
-#ifdef
+
+#define cc_sllst(_cc_sllst_object,                                         \
+                 _cc_sllst_element_type)                                   \
+                                                                           \
+struct                                                                     \
+{                                                                          \
+    int size;                                                              \
+    void *head;                                                            \
+}   _cc_sllst_object = _cc_sllst_init;                                     \
+                                                                           \
+typedef _cc_sllst_element_type _cc_sllst_object##_element_type
+
+
+#define _cc_sllst_node(_cc_sllst_object) _cc_xllst_node(_cc_sllst_object)
+
+
+
+#endif
