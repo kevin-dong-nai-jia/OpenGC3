c2139291091fabfc29e5de1c6b0f297a36e2743e
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-01-18 04:22:01 +0800

    add __cc_error_dllst_iter_cannot_dereference

--- src/ccc/debug.h
+++ src/ccc/debug.h
@@ -7,7 +7,7 @@
 
 
 
-void* __cc_message(char* file, int line, char* type, int count, ...)
+void* __cc_message(char* file, int line, char* type, int code, int count, ...)
 {
     #ifdef CCC_DEBUG
 
@@ -23,12 +23,23 @@ void* __cc_message(char* file, int line, char* type, int count, ...)
 
     va_end(ap);
 
-    if (strcmp(type, "ERROR") == 0)
-        exit(-1);
+        #ifdef CCC_DEBUG_STRICT
+
+        if (strcmp(type, "ERROR") == 0)
+        {
+            fprintf(stderr, "[ CCC ] TERMINATED: return status %d\n\n", code);
+            exit(code);
+        }
+
+        #else
+
+        (void)code;
+
+        #endif
 
     #else
 
-    (void)file, (void)line, (void)type, (void)count;
+    (void)file, (void)line, (void)type, (void)code, (void)count;
 
     #endif
 
@@ -36,17 +47,33 @@ void* __cc_message(char* file, int line, char* type, int count, ...)
 }
 
 
-#define __cc_warning_dllst_empty(_cc_dllst_object)                         \
+
+/* CCC Warning (print the warning message if CCC_DEBUG is set) */
+
+
+#define __cc_warning_dllst_is_empty(_cc_dllst_object)                      \
 (                                                                          \
-    __cc_message(__FILE__, __LINE__, "WARNING", 3,                         \
-                 #_cc_dllst_object, "(a dllst container)", "is empty.")    \
+    __cc_message(__FILE__, __LINE__, "WARNING", 0,                         \
+                 3, #_cc_dllst_object, "(a dllst container)", "is empty.") \
 )
 
 
 #define __cc_warning_dllst_iter_is_invalid(_cc_dllst_iter)                 \
 (                                                                          \
-    __cc_message(__FILE__, __LINE__, "WARNING", 3,                         \
-                 #_cc_dllst_iter, "(a dllst iterator)", "is invalid.")     \
+    __cc_message(__FILE__, __LINE__, "WARNING", 0,                         \
+                 3, #_cc_dllst_iter, "(a dllst iterator)", "is invalid.")  \
+)
+
+
+
+/* CCC Error (terminate the process if CCC_DEBUG_STRICT is set) */
+
+
+#define __cc_error_dllst_iter_cannot_dereference(_cc_dllst_iter)           \
+(                                                                          \
+    __cc_message(__FILE__, __LINE__, "ERROR", -1,                          \
+                 3, #_cc_dllst_iter, "(a dllst iterator)",                 \
+                 "cannot be dereferenced.")                                \
 )
 
 
