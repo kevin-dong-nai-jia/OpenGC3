22f1f4e84d934a73af4f3eb53ea9075ec488b310
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-02-10 00:58:03 +0800

    add __cc_error_dllst_failed_to_alloc

--- src/ccc/message.h
+++ src/ccc/message.h
@@ -21,12 +21,28 @@ void *__cc_message(char* file, int line, char* type, int count, ...)
 
     va_end(ap);
 
+    if (strcmp(type, "ERROR") == 0)
+        exit(EXIT_FAILURE);
+
     return NULL;
 }
 
 
 
-/* CCC Warning (print the warning message if CCC_MESSAGE is set) */
+/* CCC Error */
+
+
+#ifdef CCC_MESSAGE
+    #define __cc_error_dllst_failed_to_alloc(_cc_dllst_object)             \
+        __cc_message(__FILE__, __LINE__, "ERROR", 3, "dllst:",             \
+                     "Failed to allocate memory for", #_cc_dllst_object)
+#else
+    #define __cc_error_dllst_failed_to_alloc(_cc_dllst_object) NULL
+#endif
+
+
+
+/* CCC Warning */
 
 
 #ifdef CCC_MESSAGE
