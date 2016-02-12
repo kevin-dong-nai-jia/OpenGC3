d022f5f78f510bb5f113d76f8add6ec020cf4967
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-02-13 03:42:30 +0800

    initial commit

---
+++ src/ccc/algorithm.h
@@ -0,0 +1,16 @@
+#ifndef _CCC_ALGORITHM_H_
+#define _CCC_ALGORITHM_H_
+
+#include "dllst.h"
+
+
+
+/* Sorting */
+
+
+#define cc_dllst_sort(_cc_dllst_object)                                    \
+    /* TODO */
+
+
+
+#endif
