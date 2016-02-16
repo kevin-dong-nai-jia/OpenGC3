0a2fae392d4fe54ea20309c0dfaa2f4fe87e2fb1
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2016-02-17 02:33:00 +0800

    add CCC_VOID_EXPR_

--- src/ccc.h
+++ src/ccc.h
@@ -3,12 +3,17 @@
 
 
 
-/* CCC statement */
+/* CCC properties */
 
 
 #define CCC_STATEMENT_(_ccc_statement)                                     \
                                                                            \
     do {_ccc_statement} while (0)
+
+
+#define CCC_VOID_EXPR_(_ccc_expression)                                    \
+                                                                           \
+    ((_ccc_expression), ((void)0))
 
 
 
