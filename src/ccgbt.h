a3bce7cfc1b776783fadae8d7968d511838fa39b
andylinpersonal <andylinpersonal@gmail.com>
2017-03-17 04:55:02 +0800

    [incomplete] constructing body struct...

--- src/ccgbt.h
+++ src/ccgbt.h
@@ -10,6 +10,72 @@
 #include <stdint.h>
 #include <string.h>
 
-// TO BE IMPLEMENTED
+
+/* synonym       */
+
+#define PRN lnk[0]
+#define LFT lnk[1]
+#define RGH lnk[2]
+
+#define CCGBT       ADDID(CCGBT)
+#define CCGBT_BODY  ADDID(CCGBT_BODY)
+#define CCGBT_NODE  ADDID(CCGBT_NODE)
+#define CCGBT_BLCK  ADDID(CCGBT_BLCK)
+#define CCGBT_ITER  ADDID(CCGBT_ITER)
+#define CCGBT_CURR  ADDID(CCGBT_CURR)
+
+
+
+/* ccgbt instance create */
+
+
+#define ccgbt(elem_t)                                                          \
+                                                                               \
+        ccgbt_extd(elem_t, 1, NORMAL)
+
+#define ccgbt_pckd(elem_t)                                                     \
+                                                                               \
+        ccgbt_extd(elem_t, 1, PACKED)
+
+#define ccgbt_extd(elem_t, _n_iter, _ALIGN_)                                   \
+                                                                               \
+        typedef ccgbt_struct_extd(elem_t, _n_iter, _ALIGN_) *CCGBT;  CCGBT
+
+
+#define ccgbt_struct(elem_t)                                                   \
+                                                                               \
+        ccgbt_struct_extd(elem_t, 1, NORMAL)
+
+#define ccgbt_struct_pckd(elem_t)                                              \
+                                                                               \
+        ccgbt_struct_extd(elem_t, 1, PACKED)
+
+#define ccgbt_struct_extd(elem_t, _n_iter, _ALIGN_)                            \
+                                                                               \
+        struct CCGBT_BODY                                                      \
+        {                                                                      \
+            /* size and node record */                                         \
+            int size,  used,  vcnt;                                            \
+                                                                               \
+            /* start: 1st alloc node num, ratio: growth ratio   */             \
+            /* thrsh: max growth num                            */             \
+            /* example: start = 2, ratio = 2, thrsh = 17        */             \
+            /* -> 2, 4, 8, 16, 32, 17, 17,...                   */             \
+            /* block increment info */                                         \
+            int start, ratio, thrsh;                                           \
+                                                                               \
+            struct CCGBT_NODE                                                  \
+            {                                                                  \
+                struct CCGBT_NODE *lnk[3];                                     \
+                elem_t val;                                                    \
+            }   *avsp, *pnode, *stnl;                                          \
+        }
+
+
+
+/* ccdll initialize */
+
+
+
 
 #endif
