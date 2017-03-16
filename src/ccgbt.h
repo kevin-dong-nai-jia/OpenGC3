9124a01222c9cd032af27ab9c6c8567f693eef4a
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2017-03-16 19:27:19 +0800

    add ccgbt{.h|-test.c}

---
+++ src/ccgbt.h
@@ -0,0 +1,15 @@
+#ifndef CC_GBT_H
+#define CC_GBT_H
+
+#include "pool.h"
+#include "misc.h"
+#include "snym.h"
+
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+// TO BE IMPLEMENTED
+
+#endif
---
+++ test/tree/ccgbt-test.c
@@ -0,0 +1,28 @@
+// #define CCC_STRICT
+
+#include "../../src/ccgbt.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+{
+    // Test 0
+    // Demonstration
+
+    printf("Test 0: \n\n");
+
+    {
+        ccgbt(char*) tree;
+        ccgbt_init(tree);
+
+        // TODO
+
+        ccgbt_free(tree);
+    }
+
+
+    return 0;
+}
