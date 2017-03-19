21f881e58cf80ee255f6a7aaf09755e8f2e2b51d
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2017-03-19 19:11:01 +0800

    add test case 1

--- test/tree/ccgbt-test.c
+++ test/tree/ccgbt-test.c
@@ -46,5 +46,45 @@ int main(void)
     }
 
 
+    // Test 1
+    // insert / push / erase / rotate ...
+
+    printf("Test 1: \n\n");
+
+    {
+        ccgbt(char) tree;
+        ccgbt_init(tree);
+
+        ccgbt_iter_root   (ITER(tree));
+        ccgbt_push_left   (ITER(tree), 'I');
+        ccgbt_push_left   (ITER(tree), 'J');
+        ccgbt_insert_right(ITER(tree), 'K');
+        ccgbt_rotate      (ITER(tree));
+
+        ccgbt_push_left   (ITER(tree), 'L');
+        ccgbt_insert_right(ITER(tree), 'M');
+
+        ccgbt_push_right  (ITER(tree), 'N');
+        ccgbt_insert_left (ITER(tree), 'O');
+
+        ccgbt_iter_right  (ITER(tree));
+        ccgbt_erase_left  (ITER(tree));
+
+        ccgbt_iter_right  (ITER(tree));
+        ccgbt_erase_right (ITER(tree));
+
+        ccgbt_iter_right  (ITER(tree));
+        ccgbt_erase_left  (ITER(tree));
+
+        ccgbt_iter_right  (ITER(tree));
+        ccgbt_erase_right (ITER(tree));
+
+        ccgbt_iter_right  (ITER(tree));
+        ccgbt_erase_left  (ITER(tree));
+
+        printf("%c%c\n", ccgbt_left(ITER(tree)), ccgbt_right(ITER(tree)));
+    }
+
+
     return 0;
 }
