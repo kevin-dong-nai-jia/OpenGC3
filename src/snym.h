c4bdf3372f27f764e04dd2771a309d0ba7fef02d
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2017-04-16 21:33:10 +0800

    add ELEM and ELEM_NTH

--- src/snym.h
+++ src/snym.h
@@ -4,16 +4,20 @@
 
 /* Dereference */
 
+// ccdll & ccxll
 #define LREF(_iter)         ((_iter)->curr.node->val)
 
+// ccdll
 #define DREF(_iter)         ((_iter)->curr.node->val)
 #define DREF_PREV(_iter)    ((_iter)->curr.node->PRV->val)
 #define DREF_NEXT(_iter)    ((_iter)->curr.node->NXT->val)
 
+// ccxll
 #define XREF(_iter)         ((_iter)->curr.node->val)
 #define XREF_PREV(_iter)    ((_iter)->prev.node->val)
 #define XREF_NEXT(_iter)    ((_iter)->next.node->val)
 
+// ccgbt
 #define GREF(_iter)         ((_iter)->curr.node->val)
 #define GREF_PARENT(_iter)  ((_iter)->curr.node->PRN->val)
 #define GREF_LEFT(_iter)    ((_iter)->curr.node->LFT->val)
@@ -22,11 +26,17 @@
 
 /* Abbreviation */
 
+// ccdll & ccxll
 #define DLEQ  ccdll_comp_leq
 #define XLEQ  ccxll_comp_leq
 
+// ccdll & ccxll
 #define ITER(_cont)               (ITER_NTH(_cont, 0))
 #define ITER_NTH(_cont, _nth_it)  (&(*(_cont)->itarr)[(_nth_it)])
+
+// ccarr
+#define ELEM(_cont)               (ELEM_NTH(_cont, 0))
+#define ELEM_NTH(_cont, _nth_el)  ((_cont).arr[(_nth_el)])
 
 
 /* Abbreviation (Internal) */
