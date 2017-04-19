a5eb9b44d6213e6e35a3fe72fd49baaa0ab71bf8
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2017-04-20 05:39:17 +0800

    minor fix and re-render

--- tool/ccxll-call.sh
+++ tool/ccxll-call.sh
@@ -2,49 +2,54 @@
 
 cd "$(dirname "$0")"
 
+HDRFILE='../src/ccxll.h'
+PDFFILE='ccxll-call.pdf'
+
+TEM_DOT='ccxll-call.tem.dot'
+TEM_SVG='ccxll-call.tem.svg'
+
 CURTIME="$(LANG=en_US date '+%b %Y')"
 
 PREPROC="$(gcc -fpreprocessed              \
-            -w -dD -E '../src/ccxll.h' |   \
+            -w -dD -E "$HDRFILE" |         \
            sed ':a;N;$!ba;s/\\\n//g' |     \
            grep ^#define |                 \
            awk '{$1="" ; print "#" $0}' |  \
            sed 's/[^#a-zA-Z0-9_]/ /g' |    \
-           tr -s ' ')"
+           sed 's/##/ /g' | tr -s ' ')"
 
 PATTERN="$(echo "$PREPROC" |               \
            awk '{print $2}' |              \
-           paste -sd '|' - |               \
-           sed 's/|/\\|/g')"
+           paste -sd "\n" && echo '#')"
 
-BLACKLS='OPENGC3_CCXLL_H\|XOR\|'`
-       `'ccxll\|CCXLL\|'`
-       `'CCXLL_CONT\|CCXLL_NODE\|'`
-       `'CCXLL_BLCK\|CCXLL_ITER\|'`
-       `'CCXLL_HDTL\|CCXLL_ADJC'
+echo "$PATTERN" > 'PATTERN'
 
-MATCHED="$(echo -n 'ccxll ' &&             \
-           echo "$PREPROC" |               \
+# SET ='@' if ignored
+BLACKLS='OPENGC3_CCXLL_H\|XOR\|ccxll\|CCXLL\|'`
+       `'CCXLL_CONT\|CCXLL_NODE\|CCXLL_BLCK\|CCXLL_ITER\|'`
+       `'CCXLL_HDTL\|CCXLL_ADJC\|ccxll_append'
+
+MATCHED="$(echo "$PREPROC" |               \
            tr ' ' '\n' |                   \
-           grep -w "#\|$PATTERN" |         \
+           LC_ALL=C grep -w -f 'PATTERN' | \
            grep -v -w "$BLACKLS" |         \
            tr '\n' ' ' |                   \
            sed 's/# /\n/g' |               \
            sed '/^$/d' && echo '')"
 
-BLACKLS="ccxll_append"
+DOTFRMT='{printf $1" -> ";$1="{";print $0" }"}'
 
-DOTFRMT='{printf "    { " $1 " } -> " ; $1="{" ; print $0 " }" }'
+WHITELS='ccxll '
 
-DIGRAPH="$(echo "$MATCHED" | grep -v -w "$BLACKLS" | awk  "$DOTFRMT")"
+DIGRAPH="$(echo "$WHITELS" "$MATCHED" | awk "$DOTFRMT")"
 
-DOTFILE="$(m4 'ccxll-call.tem.dot' && echo -e "$DIGRAPH" '\n }')"
+DOTFILE="$(m4 "$TEM_DOT" && echo -e "$DIGRAPH" '\n } ')"
 
-SVGFILE="$(dot -Tsvg <<< "$DOTFILE" | head -n -1 && \
-           cat 'ccxll-call.tem.svg' | sed "s/\[CURTIME\]/$CURTIME/g")"
+SVGFILE="$(dot -Tsvg <<< "$DOTFILE" | head -n -1 &&   \
+           cat "$TEM_SVG" | sed "s/\[CURTIME\]/$CURTIME/g")"
 
-SVG2PDF="$(rsvg-convert -f 'pdf' <<< "$SVGFILE"  > 'ccxll-call')"
+SVG2PDF="$(rsvg-convert -f 'pdf' <<< "$SVGFILE" >'PDF_RAW')"
 
-PDFCROP="$(pdfcrop --margins '64' 'ccxll-call' 'ccxll-call.pdf')"
+PDFCROP="$(pdfcrop --margins '64' 'PDF_RAW' "$PDFFILE")"
 
-rm 'ccxll-call'
+rm 'PATTERN' 'PDF_RAW'
