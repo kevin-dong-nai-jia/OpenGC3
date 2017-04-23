49c34864053f985cd83cc17eb765a727af27ed62
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2017-04-23 12:35:07 +0800

    minor fix and re-render

--- tool/ccxll-call.sh
+++ tool/ccxll-call.sh
@@ -7,58 +7,49 @@ cd "$(dirname "$0")"
 
 HDRFILE='../src/ccxll.h'
 PDFFILE='ccxll-call.pdf'
-
 TEM_DOT='ccxll-call.tem.dot'
 TEM_SVG='ccxll-call.tem.svg'
+
+CURTIME="$(LANG=en_US date '+%b %Y')"
 
 RM_WORD='OPENGC3_CCXLL_H\|XOR\|ccxll\|CCXLL\|'`
        `'CCXLL_CONT\|CCXLL_NODE\|CCXLL_BLCK\|'`
        `'CCXLL_ITER\|CCXLL_HDTL\|CCXLL_ADJC\|'`
        `'ccxll_empty\|ccxll_size\|ccxll_append\|'`
        `'ccxll_front\|ccxll_back\|ccxll_comp_leq'
+PREPEND='ccxll ccxll_extd\n'
 
 
-# CALL-GRAPH GENERATION
+# CALL GRAPH GENERATION
 
-CURTIME="$(LANG=en_US date '+%b %Y')"
+RM_LINE="$(echo "$RM_WORD"  | \
+           sed 's/\\|/\n/g' | sed 's/^/# /' | paste -sd '|' | sed 's/|/\\|/g')"
 
-RM_LINE="$(echo "$RM_WORD" |                \
-           sed 's/\\|/\n/g' |               \
-           sed 's/^/# /' | paste -sd '|' |  \
-           sed 's/|/\\|/g')"
-
-PREPROC="$(gcc -fpreprocessed               \
-            -w -dD -E "$HDRFILE" |          \
-           sed ':a;N;$!ba;s/\\\n//g' |      \
-           grep ^#define |                  \
-           awk '{$1="" ; print "#" $0}' |   \
-           sed 's/[^#a-zA-Z0-9_]\|##/ /g' | \
+PREPROC="$(gcc -fpreprocessed -w -dD -E "$HDRFILE"      | \
+           sed ':a;N;$!ba;s/\\\n//g'    | grep ^#define | \
+           awk '{$1="" ; print "#" $0}' | sed 's/[^#a-zA-Z0-9_]\|##/ /g' | \
            grep -v -w "$RM_LINE")"
 
-KEYWORD="$(echo "$PREPROC" |                \
-           awk '{print $2}' && echo '#')"
+{ echo "$PREPROC" | awk '{print $2}' && echo '#' ; } > 'KEYWORD'
 
-echo "$KEYWORD" > 'KEYWORD'
-
-MATCHED="$(echo "$PREPROC" |                \
-           tr -s ' ' | tr ' ' '\n' |        \
-           LC_ALL=C grep -w -f 'KEYWORD' |  \
-           tr '\n' ' ' | sed 's/# /\n/g' |  \
-           sed '/^$/d' && echo '')"
+MATCHED="$(echo "$PREPROC" | \
+           tr -s ' '   | tr  ' ' '\n'    | LC_ALL=C grep -w -f 'KEYWORD' | \
+           tr '\n' ' ' | sed 's/# /\n/g' | sed '/^$/d')"
 
 DOTFRMT='{printf $1" -> ";$1="{";print $0" }"}'
 
-PREPEND=$'ccxll ccxll_extd\n'
+DIGRAPH="$(echo -e "$PREPEND" "$MATCHED" | awk "$DOTFRMT")"
 
-DIGRAPH="$(echo "$PREPEND" "$MATCHED" | awk "$DOTFRMT")"
+DOTFILE="$(m4 "$TEM_DOT" && echo -e "$DIGRAPH" '\n}')"
 
-DOTFILE="$(m4 "$TEM_DOT" && echo -e "$DIGRAPH" '\n } ')"
-
-SVGFILE="$(dot -Tsvg <<< "$DOTFILE" | head -n -1 &&   \
+SVGFILE="$(dot -Tsvg <<< "$DOTFILE" | head -n -1 && \
            cat "$TEM_SVG" | sed "s/\[CURTIME\]/$CURTIME/g")"
 
-SVG2PDF="$(rsvg-convert -f 'pdf' <<< "$SVGFILE" >'PDF_RAW')"
+rsvg-convert -f 'pdf' <<< "$SVGFILE" > "$PDFFILE"
 
-PDFCROP="$(pdfcrop --margins '64' 'PDF_RAW' "$PDFFILE")"
+rm 'KEYWORD'
 
-rm 'KEYWORD' 'PDF_RAW'
+
+# KNOWN ISSUE
+
+## The parser doesn't detect whether the token follows an enclosed argument list
