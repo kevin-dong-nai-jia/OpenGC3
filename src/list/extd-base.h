68b486661cfccde772616aa7990907b527154d7e
Kevin Dong Nai Jia <kevin.dong.nai.jia@gmail.com>
2018-02-12 03:32:30 +0800

    edit cc_ll_is_robust

--- src/list/extd-base.h
+++ src/list/extd-base.h
@@ -31,19 +31,24 @@ STATEMENT_                                                                     \
         if (_ll_##_iter_at_end(_it((_cc_ll), _base_s3, 1)))  break;            \
     }                                                                          \
                                                                                \
-    _it_clear((_cc_ll), 2);                                                    \
+    _it_clear((_cc_ll), 2, , );                                                \
 )
 
 
-#define cc_ll_is_robust(_cc_ll, _ptrue, _ll_, _LL_)          /* INCOMPLETE */  \
+#define cc_ll_is_robust(_cc_ll, _ptrue, _ll_, _LL_)                            \
                                                                                \
 STATEMENT_                                                                     \
 (                                                                              \
-    int _size = 0;                                                             \
+    int _size;                                                                 \
+    *(_ptrue) = 1;                                                             \
                                                                                \
+    _size = 0;                                                                 \
     _LL_##_INCR((_cc_ll)->_iter)  _size++;                                     \
+    *(_ptrue) &= !!(_ll_##_size((_cc_ll)) == _size);                           \
                                                                                \
-    *(_ptrue) = !!(_ll_##_size((_cc_ll)) == _size);                            \
+    _size = 0;                                                                 \
+    _LL_##_DECR((_cc_ll)->_iter)  _size++;                                     \
+    *(_ptrue) &= !!(_ll_##_size((_cc_ll)) == _size);                           \
 )
 
 
